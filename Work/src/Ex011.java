a1aff14dcc30ee58a0e074bf73a54159e397e7c3
104021055 <104021055@live.asia.edu.tw>
2015-09-17 19:52:42 +0800

    0917

---
+++ Work/src/Ex011.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Ex011 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn=new Scanner(System.in);
+		int n=scn.nextInt();
+		int m=scn.nextInt();
+		System.out.println("©M:"+(n+m));
+		System.out.println("®t:"+(n-m));
+		System.out.println("¿n:"+(n*m));
+
+	}
+
+}
---
+++ Work/src/Ex012.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Ex012 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn=new Scanner(System.in);
+		float c=scn.nextFloat();
+		float e=scn.nextFloat();
+		float m=scn.nextFloat();
+		System.out.print((c+e+m)+"\t"+((c+e+m)/3));
+
+	}
+
+}
